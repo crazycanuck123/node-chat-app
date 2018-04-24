dd328f44984506b67c98bffa9bf7d933747235d5
matthewcoledesign@gmail.com <matthewcoledesign@gmail.com>
2018-04-23 20:12:35 -0600

    created basics for Chat App

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+
+app.get('/', (req, res) =>{
+
+  res.render('index.html');
+  });
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
