dae3bd8caead950e800e917f8ca7b3df7ea43051
matthewcoledesign@gmail.com <matthewcoledesign@gmail.com>
2018-07-25 11:54:18 -0600

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -14,11 +14,13 @@
 
   socket.on('newMessage', function(message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    console.log('Message', message);
-    var li = $('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-    $('#messages').append(li);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+      text: message.text,
+      from: message.from,
+      createdAt: formattedTime
+    });
+    $('#messages').append(html);
   });
 
   socket.on('newUser', function(message) {
@@ -30,14 +32,14 @@
 
   socket.on('newLocationMessage', function(message){
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My current location</a>');
-
-    li.text(`${message.from} ${formattedTime}: `);
-    a.attr('href', message.url);
-    li.append(a);
-
-    $('#messages').append(li);
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+      text: message.text,
+      from: message.from,
+      createdAt: formattedTime,
+      url: message.url
+    });
+    $('#messages').append(html);
   });
 
   var messageTextbox = $('[name=message]');
